e0a48acec5f789492470d83ae1b98095b953316c
matevzkmetec <35583240+matevzkmetec@users.noreply.github.com>
2018-01-21 20:18:41 +0100

    Add files via upload

---
+++ src/Controller/WhenController.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\when_counter\Controller\WhenController.
+ */
+namespace Drupal\when_counter\Controller;
+class WhenController {
+  public function content() {
+    return array(
+      '#type' => 'markup',
+    );
+  }
+}
